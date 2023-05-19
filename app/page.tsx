b195b73c6a64f6378c0a42200428d39ae40624db
Ankan <ankanbhattacharya89@gmail.com>
2023-05-19 13:25:26 +0530

    feat: :sparkles: add hero image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import HeroImage from "@/assets/my-id-hero.png";
+import Image from "next/image";
+
 const Home = () => {
     return (
-        <main className="min-h-screen bg-gray-200 flex flex-col w-full">
-            <h1 className="text-black">Hello There</h1>
+        <main className="min-h-screen bg-gray-200 flex flex-col items-center justify-center w-full">
+            <Image src={HeroImage.src} width={400} height={400} alt="" className="w-96 h-96 object-contain"/>
         </main>
     );
 };
